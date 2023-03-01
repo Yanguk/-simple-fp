52c054d7ca55ea2f4db3c18fa3ef6bba46ec0c5f
Yanguk <yanguk.heo@knowre.com>
2023-03-01 21:17:52 +0900

    feature: Some, None 추가

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export { default as go } from './common/go';
 export { default as pipe } from './common/pipe';
-export { default as Option } from './common/Option';
+export { default as Option, Some, None } from './common/Option';

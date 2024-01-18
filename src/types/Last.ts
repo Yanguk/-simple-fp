a3ba59f9184d6f8280934f763b5459e73076290d
Yanguk <yanguk.heo@knowre.com>
2024-01-18 22:41:24 +0900

    Add: util-types 추가

---
+++ src/types/Last.ts
@@ -0,0 +1,13 @@
+export type Last<T extends any[]> = 
+	T extends [infer First, ...infer Rest]
+    ? Rest extends []
+        ? First
+        : Last<Rest>
+    : never;
+
+
+/**
+ *
+ * type test = Last<[3, 2, 1]>; // 1
+ *
+ */
